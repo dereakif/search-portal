926c84fec5a0de4dc419b4fb251f541bbc15a680
Akif <dereakif@gmail.com>
2022-02-18 21:31:56 +0300

    initial commit

---
+++ pages/api/search.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-anonymous-default-export */
+import mockData from "../../mockData.json";
+
+export default (req, res) => {
+  console.log(req.query, "req");
+  const filter = req.query.q ? new RegExp(req.query.q, "i") : /.*/;
+  console.log({ filter });
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(
+    JSON.stringify(
+      mockData.data.filter((info) => info[0].match(filter)).slice(0, 40)
+    )
+  );
+};
