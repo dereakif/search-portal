fcf77617cf3119e6e7325073b29584bbe8cc12d3
Akif <dereakif@gmail.com>
2022-02-21 09:59:05 +0300

    add: pagination, implement: query

--- pages/results.js
+++ pages/results.js
@@ -1,31 +1,37 @@
 import axios from "axios";
 import { useQuery } from "react-query";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import styles from "../styles/Results.module.scss";
+import { useRouter } from "next/router";
 
-const getResults = async (name) => {
+const getResultsOnSSR = async (name) => {
   const { data } = await axios.get(
     `http://localhost:3000/api/results?name=${name}`
   );
   return data;
 };
 
-const search = async (filter) => {
-  const name = filter.queryKey[1];
-  const { data } = await axios.get(`/api/search?q=${name}`);
+const getResultsOnCS = async (filter) => {
+  const { name, page, orderBy, ascending } = filter.queryKey[1];
+  const { data } = await axios.get(
+    `/api/results?name=${name}&page=${page}&orderBy=${orderBy}&ascending=${ascending}`
+  );
   return data;
 };
 
 export async function getServerSideProps(context) {
   const initialName = context.query.name;
-  const initialData = await getResults(initialName);
+  const initialData = await getResultsOnSSR(initialName);
+  if (!initialData || !initialName) {
+    return { props: {} };
+  }
   return {
     props: { initialData, initialName },
   };
 }
 const renderResult = (data) => {
-  if (!data || data?.length === 0) return;
-  return data.map((items) => (
+  if (!data || data?.results.length === 0) return;
+  return data.results.map((items) => (
     <div key={items[2]} className={styles.resultItems}>
       <div>
         <p>{items[4] + " - " + items[5]}</p>
@@ -37,34 +43,109 @@ const renderResult = (data) => {
 };
 
 export default ({ initialData, initialName }) => {
-  const [query, setQuery] = useState(initialName);
-  const [dataToShow, setDataToShow] = useState(initialData);
-  const { data, refetch, isError, isLoading } = useQuery(["q", query], search, {
-    enabled: false,
+  const router = useRouter();
+  const [query, setQuery] = useState({
+    name: initialName,
+    page: 1,
+    orderBy: "",
+    ascending: false,
   });
-
-  useEffect(() => {
-    if (query !== initialName && data?.length > 0) {
-      console.log(query);
-      setDataToShow(data);
-      setQuery("");
+  const { data, refetch, isError, isLoading } = useQuery(
+    ["q", query],
+    getResultsOnCS,
+    {
+      enabled: false,
     }
+  );
+  let dataToShow = useMemo(() => {
+    if (!data) return initialData;
+    return data;
   }, [data]);
+  const handleNext = () => {
+    if (data.pageCount > query.page) {
+      setQuery((prev) => ({ ...prev, page: prev.page + 1 }));
+    }
+  };
+  const handlePrev = () => {
+    if (query.page > 1) {
+      setQuery((prev) => ({ ...prev, page: prev.page - 1 }));
+    }
+  };
+  const handleFirst = () => {
+    setQuery((prev) => ({ ...prev, page: 1 }));
+  };
+  const search = async () => {
+    await refetch();
+    router.push(
+      `/results?name=${query.name}&page=${query.page}&orderBy=${query.orderBy}&ascending=${query.ascending}`,
+      undefined,
+      { shallow: true }
+    );
+  };
+  useEffect(() => {
+    search();
+  }, [query.orderBy, query.page, query.ascending]);
 
   return (
     <div className={styles}>
       <img alt="logo" src="/logo.jpg" />
       <input
-        value={query}
-        onChange={(evt) => setQuery(evt.target.value)}
+        value={query.name}
+        onChange={(evt) => setQuery((s) => ({ ...s, name: evt.target.value }))}
         onKeyDown={(evt) => {
-          evt.code === "Enter" && refetch();
+          evt.code === "Enter" && search();
         }}
       />
-      <button onClick={refetch}>Search</button>
-      {isError && <div>Something went wrong</div>}
-      {isLoading && <div>Loading...</div>}
-      <div className={styles.results}>{renderResult(dataToShow)}</div>
+      <button onClick={search}>Search</button>
+
+      <div className="orderByContainer">
+        <p>Order by</p>
+        <ul className={styles.orderList}>
+          <li
+            className={styles.orderListItem}
+            onClick={() => {
+              setQuery((s) => ({ ...s, orderBy: "name", ascending: true }));
+            }}
+          >
+            Name ascending
+          </li>
+          <li
+            className={styles.orderListItem}
+            onClick={() => {
+              setQuery((s) => ({ ...s, orderBy: "name", ascending: false }));
+            }}
+          >
+            Name descending
+          </li>
+          <li
+            className={styles.orderListItem}
+            onClick={() => {
+              setQuery((s) => ({ ...s, orderBy: "year", ascending: true }));
+            }}
+          >
+            Year ascending
+          </li>
+          <li
+            className={styles.orderListItem}
+            onClick={() => {
+              setQuery((s) => ({ ...s, orderBy: "year", ascending: false }));
+            }}
+          >
+            Year descending
+          </li>
+        </ul>
+      </div>
+      <div>
+        {isError && "Something went wrong"
+          ? isLoading && "Loading..."
+          : renderResult(dataToShow)}
+      </div>
+      <div className="pagination">
+        <button onClick={handleFirst}>first</button>
+        <button onClick={handlePrev}>prev</button>
+        <span>{query.page}</span>
+        <button onClick={handleNext}>next</button>
+      </div>
     </div>
   );
 };
