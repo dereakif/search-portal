ed6627a2c0378929dfe05e85863883e37276ac0e
Akif <dereakif@gmail.com>
2022-02-21 09:58:03 +0300

    refactor: api for pagination and filter

--- pages/api/results.js
+++ pages/api/results.js
@@ -1,20 +1,53 @@
 import mockData from "../../mockData.json";
+
+const sortByName = (a, b) => {
+  const nameA = a[0].toUpperCase();
+  const nameB = b[0].toUpperCase();
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  return 0;
+};
+
+const sortByYear = (a, b) => {
+  const yearA = parseInt(a[3].slice(a[3].length - 4));
+  const yearB = parseInt(b[3].slice(b[3].length - 4));
+  return yearA - yearB;
+};
+
 export default (req, res) => {
   if (!req.query.name) {
     res.statusCode = 400;
     res.send("Must have name!");
   } else {
-    const filter = req.query.name ? new RegExp(req.query.name, "i") : /.*/;
-    const results = mockData.data
-      .filter((info) => info[0].match(filter))
-      .slice(0, 6);
+    const { name, page, orderBy, ascending } = req.query;
+    const filter = name ? new RegExp(name, "i") : /.*/;
+    const data = mockData.data;
+    const total = data.filter((info) => info[0].match(filter));
+    if (orderBy === "name") {
+      total.sort(sortByName);
+    }
+    if (orderBy === "year") {
+      total.sort(sortByYear);
+    }
+    if (ascending === "false") {
+      total.reverse();
+    }
+    const pageSize = 3;
+    const pageIndex = parseInt(page) || 1;
+    const start = (pageIndex - 1) * pageSize;
+    const results = total.slice(start, start + pageSize);
     if (results.length === 0) {
       res.statusCode = 404;
       res.send("Not found!");
     } else {
       res.statusCode = 200;
       res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(results));
+      res.end(JSON.stringify({ results, pageCount: total.length / 3 }));
     }
   }
 };
