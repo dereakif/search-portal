5aac7092d78ef98ae18c693c4879676d0f2e5f68
Akif <dereakif@gmail.com>
2022-02-23 01:55:28 +0300

    add: query options

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import "../styles/globals.css";
+import "../styles/globals.scss";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnmount: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
 function MyApp({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
