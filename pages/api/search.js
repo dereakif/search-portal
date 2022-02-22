c762d7ec7d928526a6c64c13c191c1dd8427c73a
Akif <dereakif@gmail.com>
2022-02-23 01:54:35 +0300

    refactor: search for 404

--- pages/api/search.js
+++ pages/api/search.js
@@ -3,12 +3,15 @@ import mockData from "../../mockData.json";
 
 export default (req, res) => {
   const filter = req.query.q ? new RegExp(req.query.q, "i") : /.*/;
-  console.log({ filter });
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.end(
-    JSON.stringify(
-      mockData.data.filter((info) => info[0].match(filter)).slice(0, 4)
-    )
-  );
+  const result = mockData.data
+    .filter((info) => info[0].match(filter))
+    .slice(0, 3);
+  if (result.length === 0) {
+    res.statusCode = 404;
+    res.send("Not found!");
+  } else {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(result));
+  }
 };
